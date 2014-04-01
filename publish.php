9d562d78e8cab1b7d1e70237ebde3d189eea61c8
Yuvi Panda <YuviPanda@gmail.com>
2014-04-01 23:42:23 +0530

    Update RSS push URL to eqiad
    Labs is no longer in pmtpa. Removing the fqdn makes it 'more portable'

--- publish.php
+++ publish.php
@@ -316,7 +316,7 @@
 		do_edit( "Wikipedia:Wikipedia Signpost/Archives/$nextissue", "{{Signpost archive|$thisissue|$nextissue|$fortnightissue}}", "(on behalf of $editor) bot creating archives of new edition of ''[[WP:SIGNPOST|The Signpost]]''" );
 
 		//Step 16: tell Yuvi's app there's a new issue
-		$http->post( 'tools-webproxy.pmtpa.wmflabs/wp-signpost/issue/update/latest', array() );
+		$http->post( 'tools-webproxy/wp-signpost/issue/update/latest', array() );
 		
 		//Step 17: permanent Single pages
 		$newtext = "{{Wikipedia:Wikipedia Signpost/Single|issuedate=$thisissue}}";
